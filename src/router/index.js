56b522f40196aa2d890d1e22a18e9537f39f2ac8
Matheus Mendes <matheuskindrow@gmail.com>
2026-05-06 11:06:01 -0300

    fix: erro de reload no github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import FreePlayView from '../views/FreePlayView.vue'
 
@@ -9,7 +9,7 @@ const HardView = () => import('../views/HardView.vue')
 const DailyView = () => import('../views/DailyView.vue')
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
